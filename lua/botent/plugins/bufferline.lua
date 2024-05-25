04a1f2c2f2afafc0c0502c6f0ea78b3f6e930ddb
botent <anil@anilperi.me>
2024-05-25 23:02:38 +0100

    bufferline offset needs to be set up

--- lua/botent/plugins/bufferline.lua
+++ lua/botent/plugins/bufferline.lua
@@ -2,14 +2,25 @@ return {
   'akinsho/bufferline.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   version = '*',
-  opts = {
-    options = {
-      mode = 'tabs',
-      diagnostics = 'nvim_lsp',
-      diagnostics_indicator = function(count, level, diagnostics_dict, context)
-        local icon = level:match 'error' and ' ' or ' '
-        return ' ' .. icon .. count
-      end,
-    },
-  },
+  config = function()
+    require('bufferline').setup {
+      options = {
+        mode = 'tabs',
+        color_icons = true,
+        offsets = {
+          filetype = 'NvimTree',
+          text = 'File Explorer',
+          text_align = 'center',
+          separator = true,
+          highlight = 'NvimTreeNormal',
+          padding = 1,
+        },
+        diagnostics = 'nvim_lsp',
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local icon = level:match 'error' and ' ' or ' '
+          return ' ' .. icon .. count
+        end,
+      },
+    }
+  end,
 }
